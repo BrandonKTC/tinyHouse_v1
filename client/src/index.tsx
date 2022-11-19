48d24c8ea11d94df6de8910122cb62a7595b0e32
brandon kwamou <b.kwamoutchamou@gmail.com>
2022-11-19 11:46:34 +0100

    fix fucking bug

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,18 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import reportWebVitals from './reportWebVitals';
-import { Listings } from './sections';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import reportWebVitals from "./reportWebVitals";
+import { Listings } from "./sections";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <Listings title="TinyHouse Listings" />
-  </React.StrictMode>
+	document.getElementById("root") as HTMLElement
 );
 
-// If you want to start measuring performance in your app, pass a function
+const client = new ApolloClient({
+	cache: new InMemoryCache(),
+	uri: "/api",
+});
+root.render(
+	<React.StrictMode>
+		<ApolloProvider client={client}>
+			<Listings title="TinyHouse Listings" />
+		</ApolloProvider>
+	</React.StrictMode>
+);
+
+// If you wa"nt to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
--- client/src/sections/Listings/Linstings.tsx
+++ client/src/sections/Listings/Linstings.tsx
@@ -21,7 +21,7 @@ const LISTINGS = gql`
 	}
 `;
 
-const DELETE_LISTINGS = gql`
+const DELETE_LISTING = gql`
 	mutation DeleteListing($id: ID!) {
 		deleteListing(id: $id) {
 			id
@@ -39,41 +39,46 @@ export const Listings = ({ title }: Props) => {
 	const [
 		deleteListing,
 		{ loading: deleteListingLoading, error: deleteListingError },
-	] = useMutation<DeleteListingData, DeleteListingVariables>(DELETE_LISTINGS);
+	] = useMutation<DeleteListingData, DeleteListingVariables>(DELETE_LISTING);
 
-	const handleDeleteListing = async (id: String) => {
+	const handleDeleteListing = async (id: string) => {
 		await deleteListing({ variables: { id } });
 		refetch();
 	};
 
 	const listings = data ? data.listings : null;
 
-	const listingsList = listings
-		? listings.map((listing, i) => {
+	const listingsList = listings ? (
+		<ul>
+			{listings.map((listing) => {
 				return (
-					<ul key={i}>
-						<li>
-							{listing.title}
-							<button onClick={() => handleDeleteListing(listing.id)}>
-								Delete a Listings!
-							</button>
-						</li>
-					</ul>
+					<li key={listing.id}>
+						{listing.title}{" "}
+						<button onClick={() => handleDeleteListing(listing.id)}>
+							Delete
+						</button>
+					</li>
 				);
-		  })
-		: null;
+			})}
+		</ul>
+	) : null;
 
-	if (loading) return <h2>Loading...</h2>;
+	if (loading) {
+		return <h2>Loading...</h2>;
+	}
 
-	if (error)
-		return <h2>Uh oh! Something went wrong - please try again later :( </h2>;
+	if (error) {
+		return <h2>Uh oh! Something went wrong - please try again later :(</h2>;
+	}
 
 	const deleteListingLoadingMessage = deleteListingLoading ? (
 		<h4>Deletion in progress...</h4>
 	) : null;
 
-	const deleteListeningErrorMessage = deleteListingError ? (
-		<h4>Uh oh! Something went wrong - please try again later :( </h4>
+	const deleteListingErrorMessage = deleteListingError ? (
+		<h4>
+			Uh oh! Something went wrong with deleting :(. Please try again soon.
+		</h4>
 	) : null;
 
 	return (
@@ -81,7 +86,7 @@ export const Listings = ({ title }: Props) => {
 			<h2>{title}</h2>
 			{listingsList}
 			{deleteListingLoadingMessage}
-			{deleteListeningErrorMessage}
+			{deleteListingErrorMessage}
 		</div>
 	);
 };
